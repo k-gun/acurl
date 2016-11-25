c2bd8a50b29fd0e4d91400ab964ad5aaaec33a94
Kerem Güneş <k-gun@mail.com>
2016-11-25 06:44:18 +0300

    Tests..

--- test/test.php
+++ test/test.php
@@ -1,10 +1,13 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');
+
 require __dir__ .'/inc.php';
 require __dir__ .'/../ACurl/Autoload.php';
 
 ACurl\Autoload::register();
 
-// $client = new ACurl\Client([
+// $client = new ACurl\Client(null, [
 //     'method' => 'GET',
 //     'uri' => 'www.google.com',
 //     'uriParams' => ['a' => 1],
@@ -12,11 +15,11 @@ ACurl\Autoload::register();
 // ]);
 // $client->send();
 
-$client = new ACurl\Client();
-// $client->send("GET https://google.com", [], null, [], ['X-foo' => 123, 'x-A'=>"aaa"]);
-// $client->send("GET https://www.facebook.com/");
-$client->sendFunc("https://ipinfo.io/", null, null, null, null, function($client, $request, $response) {
-    prs($response->getStatus());
-});
+// $client = new ACurl\Client('get >> http://localhost/');
+$client = new ACurl\Client('http://google.com:5984/');
+$client->setOption(CURLOPT_CONNECTTIMEOUT, 1);
+$client->get();
 
-// prs($client->response->getStatus());
+prs($client->request->getHeadersRaw());
+prs($client->response->getHeadersRaw());
+var_dump($client->getFailText());
