276bfe6bc02bbd28a3b124839712289569882f5f
Kerem Güneş <k-gun@mail.com>
2016-11-26 03:52:51 +0300

    Tests..

--- test/test.php
+++ test/test.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 'On');
+ini_set('error_reporting', E_ALL);
 
 require __dir__ .'/inc.php';
 require __dir__ .'/../ACurl/Autoload.php';
@@ -16,10 +16,9 @@ ACurl\Autoload::register();
 // $client->send();
 
 // $client = new ACurl\Client('get >> http://localhost/');
-$client = new ACurl\Client('http://google.com:5984/');
-$client->setOption(CURLOPT_CONNECTTIMEOUT, 1);
-$client->get();
+$client = new ACurl\Client('get >> http://www.google.com.tr/');
+$client->send();
 
+prs($client->request->getMethod());
 prs($client->request->getHeadersRaw());
 prs($client->response->getHeadersRaw());
-var_dump($client->getFailText());
