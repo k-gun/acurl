c9a42a56ee0eee301d398f8f9745c5c30ba8a9d9
Kerem Güneş <k-gun@mail.com>
2016-11-30 02:36:43 +0300

    Add docs.

--- ACurl/Http/Response.php
+++ ACurl/Http/Response.php
@@ -1,8 +1,32 @@
 <?php
+/**
+ * Copyright 2015 Kerem Güneş
+ *    <k-gun@mail.com>
+ *
+ * Apache License, Version 2.0
+ *    <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 declare(strict_types=1);
 
 namespace ACurl\Http;
 
+/**
+ * @package ACurl
+ * @object  ACurl\Response
+ * @author  Kerem Güneş <k-gun@mail.com>
+ */
 final class Response extends Stream
 {
     /**
@@ -77,21 +101,36 @@ final class Response extends Stream
           HTTP_VERSION_NOT_SUPPORTED       = 505,
           BANDWIDTH_LIMIT_EXCEEDED         = 509;
 
+    /**
+     * Constructor.
+     */
     final public function __construct()
     {
         $this->type = StreamInterface::TYPE_RESPONSE;
     }
 
+    /**
+     * Get status.
+     * @return string
+     */
     final public function getStatus(): string
     {
         return (string) $this->getHeader('_status');
     }
 
+    /**
+     * Get status code.
+     * @return int
+     */
     final public function getStatusCode(): int
     {
         return (int) $this->getHeader('_status_code');
     }
 
+    /**
+     * Get status text.
+     * @return string
+     */
     final public function getStatusText(): string
     {
         return (string) $this->getHeader('_status_text');
