35f570c1b5a878c633cb77c811e0bb987318184d
Kerem Güneş <k-gun@mail.com>
2016-11-30 03:21:20 +0300

    Add docs.

--- ACurl/ClientBase.php
+++ ACurl/ClientBase.php
@@ -1,18 +1,65 @@
 <?php
+/**
+ * Copyright 2015 Kerem Güneş
+ *    <k-gun@mail.com>
+ *
+ * Apache License, Version 2.0
+ *    <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 declare(strict_types=1);
 
 namespace ACurl;
+
 use ACurl\Http\Request;
 use ACurl\Http\Response;
 
-// client icine tasi en sonra isterse?
+/**
+ * @package ACurl
+ * @object  ACurl\ClientBase
+ * @author  Kerem Güneş <k-gun@mail.com>
+ */
 abstract class ClientBase
 {
+    /**
+     * Version.
+     * @const string
+     */
     const VERSION = '2.0.0';
 
+    /**
+     * Request.
+     * @var ACurl\Http\Request
+     */
     protected $request;
+
+    /**
+     * Response.
+     * @var ACurl\Http\Response
+     */
     protected $response;
+
+    /**
+     * Options.
+     * @var array
+     */
     protected $options = [];
+
+    /**
+     * Options default.
+     * @var array
+     */
     protected $optionsDefault = [
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_HEADER         => true,
@@ -20,11 +67,32 @@ abstract class ClientBase
         CURLOPT_HTTPHEADER     => ['Expect:'],
         CURLINFO_HEADER_OUT    => true,
     ];
+
+    /**
+     * Autoclose.
+     * @var bool
+     */
     protected $autoClose = true;
 
+    /**
+     * cURL handler.
+     * @var resource
+     */
     protected $ch;
+
+    /**
+     * Info
+     * @var array|null
+     */
     protected $info;
 
+    /**
+     * Set magic.
+     * @param string $key
+     * @param any    $value
+     * @return void
+     * @throws \Exception
+     */
     final public function __set(string $key, $value)
     {
         switch ($key) {
@@ -37,6 +105,13 @@ abstract class ClientBase
                 throw new \Exception("'{$key}' property doesn't exists on this object!");
         }
     }
+
+    /**
+     * Get magic.
+     * @param  string $key
+     * @return any
+     * @throws \Exception
+     */
     final public function __get(string $key)
     {
         switch ($key) {
@@ -50,55 +125,105 @@ abstract class ClientBase
         }
     }
 
+    /**
+     * Get request.
+     * @return ACurl\Http\Request
+     */
     final public function getRequest(): Request
     {
         return $this->request;
     }
+
+    /**
+     * Get response.
+     * @return ACurl\Http\Response
+     */
     final public function getResponse(): Response
     {
         return $this->response;
     }
 
+    /**
+     * Set option.
+     * @param  int $key
+     * @param  any $value
+     * @return self
+     */
     final public function setOption(int $key, $value): self
     {
         $this->options[$key] = $value;
+
         return $this;
     }
 
+    /**
+     * Set options.
+     * @param  array $options
+     * @return self
+     */
     final public function setOptions(array $options): self
     {
         foreach ($options as $key => $value) {
             $this->setOption($key, $value);
         }
+
         return $this;
     }
 
-    final public function getOption($key, $valueDefault = null)
+    /**
+     * Get optionç
+     * @param  int      $key
+     * @param  any|null $valueDefault
+     * @return any|null
+     */
+    final public function getOption(int $key, $valueDefault = null)
     {
         return $this->options[$key] ?? $valueDefault;
     }
 
+    /**
+     * Get options.
+     * @return array
+     */
     final public function getOptions(): array
     {
         return $this->options;
     }
 
+    /**
+     * Set auto close.
+     * @param  bool $autoClose
+     * @return self
+     */
     final public function setAutoClose(bool $autoClose): self
     {
         $this->autoClose = $autoClose;
+
         return $this;
     }
 
+    /**
+     * Get auto close.
+     * @return bool
+     */
     final public function getAutoClose(): bool
     {
         return $this->autoClose;
     }
 
+    /**
+     * Get info.
+     * @param  string|null $key
+     * @param  any|null    $valueDefault
+     * @return any|null
+     */
     final public function getInfo(string $key = null, $valueDefault = null)
     {
+        // get all info
         if ($key === null) {
             return $this->info;
         }
+
         return $this->info[$key] ?? $valueDefault;
     }
 }
